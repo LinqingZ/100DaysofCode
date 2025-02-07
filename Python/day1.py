50475dad5204f91eecf47bd462c14419531c8ed3
LinqingZ <linqing201@gmail.com>
2025-02-06 22:42:59 -0500

    Day 1

---
+++ Python/day1.py
@@ -0,0 +1,13 @@
+print("Hello World\n")
+print("Hello " + "World")
+print("\\n") # \n
+
+print("Hello "+input("What is your name? ") + "!") # # input function prompt for the user
+# Run the input function first then concat the hello and print
+
+# len() return the length of a string
+
+print("Welcome!")
+city = input("Which city did you grow up in? ")
+pet = input("What is the name of a pet? ")
+print(f'Your band name could be {city} {pet}\n')
